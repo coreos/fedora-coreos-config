d9711ca475b51a0255cd1dfd48d46d8ba1c3d752
Benjamin Gilbert <bgilbert@redhat.com>
2019-09-04 14:29:13 -0400

    overlay/05core: set up /sysroot on live systems
    If /root.squashfs exists in the initramfs, do the following:
    
    1. Mount /root.squashfs on /sysroot
    2. Locate the ostree deployment, add a corresponding ostree= argument
       to /proc/cmdline by bind-mounting over it, run
       ostree-prepare-root.service to set up the deployment, and unmount
       /proc/cmdline
    3. Mount a tmpfs on /writable
    4. Create needed directories in /writable
    5. Create a writable overlay on /sysroot/etc, with /sysroot/etc as
       lowerdir and upper/work dirs in /writable
    6. Bind-mount /writable/var to /sysroot/var
    7. Run coreos-populate-var.service to populate /sysroot/var
    
    Disable mounting of stateroot /var on live systems, since that's handled
    by step 6.

---
+++ overlay.d/05core/usr/lib/dracut/modules.d/20live/ostree-cmdline.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# With live PXE there's no ostree= argument on the kernel command line, so
+# we need to find the tree path and pass it to ostree-prepare-root.  But
+# ostree-prepare-root only knows how to read the path from
+# /proc/cmdline, so we need to synthesize the proper karg and bind-mount
+# it over /proc/cmdline.
+# https://github.com/ostreedev/ostree/issues/1920
+
+set -euo pipefail
+
+case "${1:-unset}" in
+    start)
+        treepath="$(echo /sysroot/ostree/boot.1/*/*/0)"
+        echo "$(cat /proc/cmdline) ostree=${treepath#/sysroot}" > /tmp/cmdline
+        mount --bind /tmp/cmdline /proc/cmdline
+        ;;
+    stop)
+        umount /proc/cmdline
+        rm /tmp/cmdline
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}" >&2
+        exit 1
+        ;;
+esac
