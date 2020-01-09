60365ca06a27e365da4bca92152676a440f92fc6
Jonathan Lebon <jonathan@jlebon.com>
2020-01-09 15:14:56 -0500

    tree: promote changes from testing at 6343c639b433696567e780a2423b1695e0ea2eda

--- overlay.d/05core/usr/lib/dracut/modules.d/20live/is-live-image.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/20live/is-live-image.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Script invoked by ignition-dracut generator to detect whether this is a
 # live system without a root device.  We can't test for /run/ostree-live
-# because it may not have been created yet.
-[ -e /root.squashfs ]
+# because it's created by a generator.
+# This file is created by coreos-assembler buildextend-live.
+test -f /etc/coreos-live-initramfs
