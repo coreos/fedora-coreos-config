3c2b2c493b29befbb96acbec859c269463ef65a6
Colin Walters <walters@verbum.org>
2020-03-25 23:59:36 +0000

    WIP: Run NM via systemd unit, don't depend on ip=dhcp kargs
    See https://github.com/coreos/ignition-dracut/issues/94
    and https://github.com/coreos/ignition/pull/948
    
    Needs pairing with a cosa PR to drop the default `ip=dhcp` kargs.
    
    And yes we really want to upstream this into NM by default or so.
    
    Co-Authored-By: Dusty Mabe <dusty@dustymabe.com>

---
+++ overlay.d/05core/usr/lib/dracut/modules.d/39coreos-ignitionnetwork/module-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+depends() {
+    echo ignition network-manager
+}
+
+install_and_enable_unit() {
+    unit="$1"; shift
+    target="$1"; shift
+    inst_simple "$moddir/$unit" "$systemdsystemunitdir/$unit"
+    mkdir -p "$initdir/$systemdsystemunitdir/$target.requires"
+    ln_r "../$unit" "$systemdsystemunitdir/$target.requires/$unit"
+}
+
+install() {
+    # We're forcibly overriding NM to be run as a service
+    install_and_enable_unit coreos-NetworkManager.service network-online.target
+}
