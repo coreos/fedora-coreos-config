1b6b15112e239e54538681c51fb37a8ea18bb77a
Dusty Mabe <dusty@dustymabe.com>
2020-06-02 14:12:31 -0400

    tree: promote changes from testing-devel at 6bb676bbb945f2e04d76319594e036c584adc1c4

---
+++ overlay.d/15fcos/usr/libexec/coreos-check-ssh-keys.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+# This script will print a message in the serial console 
+# if no ssh keys were added by ignition/afterburn.
+main() {
+    # Change the output color to yellow
+    warn='\033[0;33m'
+    # No color
+    nc='\033[0m'
+
+    # See https://github.com/coreos/ignition/pull/964 for the MESSAGE_ID 
+    # source. It will track the authorized-ssh-keys entries in journald 
+    # provided via ignition.
+    ignitionusers=$(
+            journalctl -o json-pretty MESSAGE_ID=225067b87bbd4a0cb6ab151f82fa364b | \
+            jq -r '.MESSAGE' | \
+            xargs -I{} echo "Ignition: {}")
+
+    # See https://github.com/coreos/afterburn/pull/397 for the MESSAGE_ID 
+    # source. It will track the authorized-ssh-keys entries in journald  
+    # provided via afterburn.
+    afterburnusers=$(
+            journalctl -o json-pretty MESSAGE_ID=0f7d7a502f2d433caa1323440a6b4190 | \
+            jq -r '.MESSAGE' | \
+            xargs -I{} echo "Afterburn: {}")
+
+    output=''
+    if [ -n "$ignitionusers" ]; then
+        output+="$ignitionusers"
+    fi
+    if [ -n "$afterburnusers" ]; then
+        output+="$afterburnusers"
+    fi
+
+    if [ -n "$output" ]; then
+        echo "$output" > /run/console-login-helper-messages/issue.d/30_ssh_authorized_keys.issue
+    else
+        echo -e "${warn}No ssh authorized keys provided by Ignition or Afterburn${nc}" \
+             > /run/console-login-helper-messages/issue.d/30_ssh_authorized_keys.issue
+    fi
+}
+
+main
