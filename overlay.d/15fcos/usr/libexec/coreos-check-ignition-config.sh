b3d52649e81d40c2f84f406f88596ec0f669781a
Sinny Kumari <sinny@redhat.com>
2020-06-15 14:23:54 +0530

    tree: promote changes from testing at ebccc0ac82a90b028623bd69212a6c0b9ef4cd61

---
+++ overlay.d/15fcos/usr/libexec/coreos-check-ignition-config.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+# The logic for the message_id is handled in 
+# https://github.com/coreos/ignition/pull/958
+# In this script, we need to capture the journald 
+# log with the particular message_id and query using 
+#`jq` utility to check if a user config is provided.
+
+# Change the output color to yellow
+warn='\033[0;33m'
+# No color
+nc='\033[0m'
+
+# See https://github.com/coreos/ignition/pull/958 for the MESSAGE_ID source.
+# It will track the journal messages related to an ignition config provided 
+# by the user.
+output=$(journalctl -o json-pretty MESSAGE_ID=57124006b5c94805b77ce473e92a8aeb | jq -s '.[] | select(.IGNITION_CONFIG_TYPE == "user")'| wc -l)
+
+if [[ $output -gt  0 ]];then
+   echo "Ignition: user provided config was applied" > /run/console-login-helper-messages/issue.d/30_ignition_config_info.issue
+else
+  echo -e "${warn}Ignition: no config provided by user${nc}" > /run/console-login-helper-messages/issue.d/30_ignition_config_info.issue
+fi
