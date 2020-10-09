e8402f8d575f7bb7861c0b61166ea06be820fb04
Sohan Kunkerkar <sohank2602@gmail.com>
2020-10-08 21:03:04 -0400

    40ignition-conf: add new base config
    Ignition now supports a `base.d` directory for base config fragments that allows FCOS and
    RHCOS to maintain the same `00-core.ign`, and facilitate the distro-specific changes by placing
    the other base config fragments in a separate `overlay` directory(https://github.com/coreos/ignition/pull/1108).
    This commit places 00-core.ign in a `base.d` directory.
    
    Also, keeping the legacy base config and the Afterburn bit as-is, otherwise, it will break CI.
    Once we have the new Ignition release, this PR (https://github.com/coreos/fedora-coreos-config/pull/717) will remove the legacy base config and revert https://github.com/coreos/fedora-coreos-config/pull/626

--- overlay.d/05core/usr/lib/dracut/modules.d/40ignition-conf/module-setup.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/40ignition-conf/module-setup.sh
@@ -7,6 +7,9 @@ depends() {
 }
 
 install() {
+    mkdir -p "$initdir/usr/lib/ignition/base.d"
+    inst "$moddir/00-core.ign" \
+        "/usr/lib/ignition/base.d/00-core.ign"
     inst "$moddir/base.ign" \
         "/usr/lib/ignition/base.ign"
 }
