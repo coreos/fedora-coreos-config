b607e4472f8659de46434284ef8eb80a333f6363
Sohan Kunkerkar <sohank2602@gmail.com>
2020-10-29 20:25:09 -0400

    15fcos: place afterburn-sshkeys.ign in 50ignition-conf-fcos
    Make use of the new support for config fragments in Ignition (coreos/ignition#1108)
    to move out the Afterburn bit added in https://github.com/coreos/fedora-coreos-config/pull/626
    to a separate overlay directory so that RHCOS can more easily avoid it.

---
+++ overlay.d/15fcos/usr/lib/dracut/modules.d/50ignition-conf-fcos/module-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+depends() {
+    echo ignition
+}
+
+install() {
+    mkdir -p "$initdir/usr/lib/ignition/base.d"
+    inst "$moddir/30-afterburn-sshkeys-core.ign" \
+        "/usr/lib/ignition/base.d/30-afterburn-sshkeys-core.ign"
+}
