966324456911b9fb009eb9ce14ad91499e004b67
Benjamin Gilbert <bgilbert@redhat.com>
2020-11-08 22:14:39 -0500

    40ignition-ostree: generalize rootfs naming to transposefs
    We'll be generalizing the rootfs save/restore code to support saving
    and restoring other partitions.  Generalize the name to "transposefs"
    and move the saved rootfs data to /run/ignition-ostree-transposefs/root.

--- overlay.d/05core/usr/lib/dracut/modules.d/40ignition-ostree/ignition-ostree-transposefs.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/40ignition-ostree/ignition-ostree-transposefs.sh
@@ -6,8 +6,9 @@ set -euo pipefail
 # "root" being set up.
 ignition_cfg=/run/ignition.json
 rootdisk=/dev/disk/by-label/root
-saved_sysroot=/run/ignition-ostree-rootfs
-partstate=/run/ignition-ostree-rootfs-partstate.json
+saved_data=/run/ignition-ostree-transposefs
+saved_root=${saved_data}/root
+partstate_root=/run/ignition-ostree-rootfs-partstate.json
 
 case "${1:-}" in
     detect)
@@ -16,30 +17,30 @@ case "${1:-}" in
             exit 0
         fi
         echo "Detected rootfs replacement in fetched Ignition config: /run/ignition.json"
-        mkdir "${saved_sysroot}"
+        mkdir "${saved_data}"
         # use 80% of RAM: we want to be greedy since the boot breaks anyway, but
         # we still want to leave room for everything else so it hits ENOSPC and
         # doesn't invoke the OOM killer
-        mount -t tmpfs tmpfs "${saved_sysroot}" -o size=80%
+        mount -t tmpfs tmpfs "${saved_data}" -o size=80%
         ;;
     save)
         mount "${rootdisk}" /sysroot
         echo "Moving rootfs to RAM..."
-        cp -aT /sysroot "${saved_sysroot}"
+        cp -a /sysroot "${saved_root}"
         # also store the state of the partition
-        lsblk "${rootdisk}" --nodeps --paths --json -b -o NAME,SIZE | jq -c . > "${partstate}"
+        lsblk "${rootdisk}" --nodeps --paths --json -b -o NAME,SIZE | jq -c . > "${partstate_root}"
         ;;
     restore)
         # This one is in a private mount namespace since we're not "offically" mounting
         mount "${rootdisk}" /sysroot
         echo "Restoring rootfs from RAM..."
-        find "${saved_sysroot}" -mindepth 1 -maxdepth 1 -exec mv -t /sysroot {} \;
+        find "${saved_root}" -mindepth 1 -maxdepth 1 -exec mv -t /sysroot {} \;
         chattr +i $(ls -d /sysroot/ostree/deploy/*/deploy/*/)
         ;;
     cleanup)
-        if [ -d "${saved_sysroot}" ]; then
-            umount "${saved_sysroot}"
-            rm -rf "${saved_sysroot}" "${partstate}"
+        if [ -d "${saved_data}" ]; then
+            umount "${saved_data}"
+            rm -rf "${saved_data}" "${partstate_root}"
         fi
         ;;
     *)
