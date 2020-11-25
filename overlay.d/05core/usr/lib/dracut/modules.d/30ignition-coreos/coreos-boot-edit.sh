49e6fd8b53dd30637d8933ac36919b1c9924465a
Kelvin Fan <kfan@redhat.com>
2020-11-25 16:51:41 -0500

    05core: Merge `coreos-inject-rootmap.service` into `coreos-boot-edit.service`
    So we don't mount `/boot` more times than we need to.

--- overlay.d/05core/usr/lib/dracut/modules.d/30ignition-coreos/coreos-boot-edit.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/30ignition-coreos/coreos-boot-edit.sh
@@ -3,6 +3,17 @@ set -euo pipefail
 
 # For a description of how this is used, see `coreos-boot-edit.service`.
 
+cmdline=( $(</proc/cmdline) )
+karg() {
+    local name="$1" value="${2:-}"
+    for arg in "${cmdline[@]}"; do
+        if [[ "${arg%%=*}" == "${name}" ]]; then
+            value="${arg#*=}"
+        fi
+    done
+    echo "${value}"
+}
+
 # Mount /boot. Note that we mount /boot but we don't unmount it because we
 # are run in a systemd unit with MountFlags=slave so it is unmounted for us.
 bootmnt=/mnt/boot_partition
@@ -15,3 +26,10 @@ mount -o rw ${bootdev} ${bootmnt}
 firstboot_network_dir_basename="coreos-firstboot-network"
 initramfs_firstboot_network_dir="${bootmnt}/${firstboot_network_dir_basename}"
 rm -vrf ${initramfs_firstboot_network_dir}
+
+# If root is specified, assume rootmap is already configured. Otherwise,
+# append rootmap kargs to the BLS configs.
+root=$(karg root)
+if [ -z "${root}" ]; then
+    /usr/bin/rdcore rootmap /sysroot --boot-mount ${bootmnt}
+fi
