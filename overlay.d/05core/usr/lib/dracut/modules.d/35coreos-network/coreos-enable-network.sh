c5580c5794a4dc7464ce9ceb1080d5dc001ab3b7
Jonathan Lebon <jonathan@jlebon.com>
2021-01-13 17:09:11 -0500

    coreos-copy-firstboot-network: run before multipathd
    `coreos-copy-firstboot-network.service` needs to mount `/boot` and
    possibly copy files out. Instead of having to deal with races against
    multipath not having yet initialized the device mapper `/boot` device,
    avoid all this by running before `multipathd.service`. Since we mount
    `/boot` read-only, there's no consistency concerns here. This matches
    what `ignition-setup-user.service` does.
    
    However, this means that we can no longer run after the later
    `coreos-enable-network.service`, which we used to so that we were sure
    to nuke any generated NM configs from the `nm-initrd-generator` run it
    triggers.
    
    Work around this by teaching `coreos-enable-network` to save network
    configs and restore them if it detects any are present.

--- overlay.d/05core/usr/lib/dracut/modules.d/35coreos-network/coreos-enable-network.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/35coreos-network/coreos-enable-network.sh
@@ -5,6 +5,8 @@ set +euo pipefail
 . /usr/lib/dracut-lib.sh
 set -euo pipefail
 
+initramfs_network_dir="/run/NetworkManager/system-connections/"
+
 dracut_func() {
     # dracut is not friendly to set -eu
     set +euo pipefail
@@ -17,6 +19,14 @@ dracut_func() {
 if ! dracut_func getargbool 0 'rd.neednet'; then
     echo "rd.neednet=1" > /etc/cmdline.d/40-coreos-neednet.conf
 
+    # Hack: if there's already network configs there, then preserve and restore
+    # it. All we want to do here is make sure that NM will be started during
+    # initqueue; we don't want to affect the actual configs it uses, which
+    # might've come from dracut-cmdline or coreos-copy-firstboot-network.
+    if [ -n "$(ls -A ${initramfs_network_dir} 2>/dev/null)" ]; then
+        mv "${initramfs_network_dir}" "${initramfs_network_dir}.bak"
+    fi
+
     # Hack: we need to rerun the NM cmdline hook because we run after
     # dracut-cmdline.service because we need udev. We should be able to move
     # away from this once we run NM as a systemd unit. See also:
@@ -24,4 +34,9 @@ if ! dracut_func getargbool 0 'rd.neednet'; then
     set +euo pipefail
     . /usr/lib/dracut/hooks/cmdline/99-nm-config.sh
     set -euo pipefail
+
+    if [ -d "${initramfs_network_dir}.bak" ]; then
+        rm -rf "${initramfs_network_dir}"
+        mv "${initramfs_network_dir}".bak "${initramfs_network_dir}"
+    fi
 fi
