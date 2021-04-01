74dc0218b3f408bec2ac27540a111405958c1136
Benjamin Gilbert <bgilbert@redhat.com>
2021-04-01 16:58:19 -0400

    check: update for FCCT rename

--- check.py
+++ check.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 #
-# Find all FCCs in the doc tree, use the podman FCCT container to run them
-# through fcct --strict, and fail on any errors.
+# Find all Butane configs in the doc tree, use the podman Butane container
+# to run them through butane --strict, and fail on any errors.
 #
-# An FCC looks like this:
+# A Butane config looks like this:
 #
 # [source,yaml]
 # ----
@@ -11,7 +11,7 @@
 # ----
 #
 # If variant: is missing, we print a warning but continue, since there
-# might be non-FCC [source,yaml] documents.
+# might be [source,yaml] documents that aren't Butane configs.
 
 import argparse
 import os
@@ -24,13 +24,13 @@ ERR = '\x1b[1;31m'
 WARN = '\x1b[1;33m'
 RESET = '\x1b[0m'
 
-container = os.getenv('FCCT_CONTAINER', 'quay.io/coreos/fcct:release')
+container = os.getenv('BUTANE_CONTAINER', 'quay.io/coreos/butane:release')
 matcher = re.compile(r'^\[source,\s*yaml\]\n----\n(.+?\n)----$',
         re.MULTILINE | re.DOTALL)
 
 parser = argparse.ArgumentParser(description='Run validations on docs.')
 parser.add_argument('-v', '--verbose', action='store_true',
-                    help='log all detected FCCs')
+                    help='log all detected Butane configs')
 args = parser.parse_args()
 
 def handle_error(e):
@@ -47,17 +47,17 @@ for dirpath, dirnames, filenames in os.walk('.', onerror=handle_error):
             filedata = fh.read()
         # Iterate over YAML source blocks
         for match in matcher.finditer(filedata):
-            fcc = match.group(1)
-            fccline = filedata.count('\n', 0, match.start(1)) + 1
-            if not fcc.startswith('variant:'):
-                print(f'{WARN}Ignoring non-FCC at {filepath}:{fccline}{RESET}')
+            bu = match.group(1)
+            buline = filedata.count('\n', 0, match.start(1)) + 1
+            if not bu.startswith('variant:'):
+                print(f'{WARN}Ignoring non-Butane YAML at {filepath}:{buline}{RESET}')
                 continue
             if args.verbose:
-                print(f'Checking FCC at {filepath}:{fccline}')
+                print(f'Checking Butane config at {filepath}:{buline}')
             result = subprocess.run(
                     ['podman', 'run', '--rm', '-i', container, '--strict'],
                     universal_newlines=True, # can be spelled "text" on >= 3.7
-                    input=fcc,
+                    input=bu,
                     stdout=subprocess.DEVNULL,
                     stderr=subprocess.PIPE)
             if result.returncode != 0:
@@ -65,6 +65,6 @@ for dirpath, dirnames, filenames in os.walk('.', onerror=handle_error):
                 # Not necessary for ANSI terminals, but required by GitHub's
                 # log renderer
                 formatted = ERR + formatted.replace('\n', '\n' + ERR)
-                print(f'{ERR}Invalid FCC at {filepath}:{fccline}:\n{formatted}{RESET}')
+                print(f'{ERR}Invalid Butane config at {filepath}:{buline}:\n{formatted}{RESET}')
                 ret = 1
 sys.exit(ret)
