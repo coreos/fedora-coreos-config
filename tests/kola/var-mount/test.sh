7ea4a30228348cf2433fd9f3d0dba1808ff992d5
Jonathan Lebon <jonathan@jlebon.com>
2021-04-09 16:42:19 -0400

    tests/kola/var-mount: test mounting on reboot
    The first boot is special. And although we try to make it like any other
    boot wrt non-root filesystems, let's verify that.

--- tests/kola/var-mount/test.sh
+++ tests/kola/var-mount/test.sh
@@ -4,8 +4,31 @@ set -xeuo pipefail
 # restrict to qemu for now because the primary disk path is platform-dependent
 # kola: {"platforms": "qemu"}
 
+ok() {
+    echo "ok" "$@"
+}
+
+fatal() {
+    echo "$@" >&2
+    exit 1
+}
+
 src=$(findmnt -nvr /var -o SOURCE)
 [[ $(realpath "$src") == $(realpath /dev/disk/by-partlabel/var) ]]
 
 fstype=$(findmnt -nvr /var -o FSTYPE)
 [[ $fstype == xfs ]]
+
+case "${AUTOPKGTEST_REBOOT_MARK:-}" in
+  "")
+      ok "mounted on first boot"
+
+      # reboot once to sanity-check we can mount on second boot
+      /tmp/autopkgtest-reboot rebooted
+      ;;
+
+  rebooted)
+      ok "mounted on reboot"
+      ;;
+  *) fatal "unexpected mark: ${AUTOPKGTEST_REBOOT_MARK}";;
+esac
