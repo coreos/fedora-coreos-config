747c5945d16765fceb1cecc7f2500d0c3117442c
Micah Abbott <miabbott@redhat.com>
2021-06-18 16:02:35 -0400

    tests: add test for watching files labeled kubernetes_file_t
    Adds a test for the ability of `systemd` to watch files
    labeled with `kubernetes_file_t`.
    
    See: https://github.com/coreos/fedora-coreos-tracker/issues/861
    See: https://github.com/containers/container-selinux/issues/135

--- tests/kola/misc-ign-ro/test.sh
+++ tests/kola/misc-ign-ro/test.sh
@@ -36,3 +36,27 @@ if [ "$context" != "system_u:object_r:net_conf_t:s0" ]; then
     fatal "SELinux context on stub-resolv.conf is wrong"
 fi
 ok "SELinux context on stub-resolv.conf is correct"
+
+# This is for verifying that `kubernetes_file_t` labeled files can be
+# watched by systemd
+# See: https://github.com/coreos/fedora-coreos-tracker/issues/861
+# See: https://github.com/containers/container-selinux/issues/135
+if [ "$(systemctl is-active kube-watch.path)" != "active" ]; then
+    fatal "kube-watch.path did not activate successfully"
+fi
+ok "kube-watch.path successfully activated"
+
+if [ "$(touch /etc/kubernetes/kubeconfig)" ]; then
+    fatal "Unable to create /etc/kubernetes/kubeconfig"
+fi
+ok "successfully created /etc/kubernetes/kubeconfig"
+
+if [ "$(systemctl is-active kube-watch.service)" != "active" ]; then
+    fatal "kube-watch.service did not successfully activate"
+fi
+ok "kube-watch.service activated successfully"
+
+if [ "$(journalctl -o cat -u kube-watch.service | sed -n 2p)" != "Found it" ]; then
+    fatal "kube-watch.service did not print message to journal"
+fi
+ok "Found message from kube-watch.service in journal"
