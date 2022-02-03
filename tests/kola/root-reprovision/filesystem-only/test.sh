aa735fb59e6bc7a8faa13a1f9e305d14c4d412ae
Dusty Mabe <dusty@dustymabe.com>
2022-02-02 22:09:08 -0500

    tests/root-reprovision: bump timeout for reprovision tests to 15m
    All of these tests include a lot of disk I/O and can take a long time.
    They all flirt with the 10 minute timeout already and some can go over.
    Let's bump them all to give us some breathing room.
    
    Also updated each test to include an explanation for the kola.json
    parameters.
    
    (cherry picked from commit 79a2fdb8ef0fb25b2e07b816d5c611617dc5c947)

--- tests/kola/root-reprovision/filesystem-only/test.sh
+++ tests/kola/root-reprovision/filesystem-only/test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-# kola: {"platforms": "qemu", "minMemory": 4096}
+# kola: { "platforms": "qemu", "minMemory": 4096, "timeoutMin": 15 }
+#
+# - platforms: qemu
+#   - This test should pass everywhere if it passes anywhere.
+# - minMemory: 4096
+#   - Root reprovisioning requires at least 4GiB of memory.
+# - timeoutMin: 15
+#   - This test includes a lot of disk I/O and needs a higher
+#     timeout value than the default.
+
 set -xeuo pipefail
 
 ok() {
--- tests/kola/root-reprovision/swap-before-root/test.sh
+++ tests/kola/root-reprovision/swap-before-root/test.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-# This test only runs on FCOS due to a problem enabling a swap partition on
-# RHCOS.  See: https://github.com/openshift/os/issues/665
-# kola: {"distros": "fcos", "platforms": "qemu", "minMemory": 4096}
+# kola: { "distros": "fcos", "platforms": "qemu", "minMemory": 4096, "timeoutMin": 15 }
+#
+# - distros: fcos
+#   - This test only runs on FCOS due to a problem enabling a swap partition on
+#     RHCOS. See: https://github.com/openshift/os/issues/665
+# - platforms: qemu
+#   - This test should pass everywhere if it passes anywhere.
+# - minMemory: 4096
+#   - Root reprovisioning requires at least 4GiB of memory.
+# - timeoutMin: 15
+#   - This test includes a lot of disk I/O and needs a higher
+#     timeout value than the default.
+
 set -xeuo pipefail
 
 ok() {
