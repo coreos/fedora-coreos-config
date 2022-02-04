7e7164ddd93d2879c47a29f17578b9e91c0cbed3
Benjamin Gilbert <bgilbert@redhat.com>
2022-02-03 21:49:30 -0500

    tests/kola/root-reprovision/swap-before-root: set allowConfigWarnings
    Starting with https://github.com/coreos/coreos-assembler/pull/2686,
    kola fails any test that produces warnings when parsing Ignition or
    Butane configs unless configured otherwise.
    
    (cherry picked from commit 60c082cad6daec3aa718d7787b665ea27e802771)

--- tests/kola/root-reprovision/swap-before-root/test.sh
+++ tests/kola/root-reprovision/swap-before-root/test.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-# kola: {"distros": "fcos", "platforms": "qemu", "minMemory": 4096}
-# This test only runs on FCOS due to a problem enabling a swap partition on
-# RHCOS.  See: https://github.com/openshift/os/issues/665
+# kola: { "distros": "fcos", "platforms": "qemu", "minMemory": 4096, "timeoutMin": 15, "allowConfigWarnings": true }
+#
+# - distros: fcos
+#   - This test only runs on FCOS due to a problem enabling a swap partition on
+#     RHCOS. See: https://github.com/openshift/os/issues/665
+# - platforms: qemu
+#   - This test should pass everywhere if it passes anywhere.
+# - minMemory: 4096
+#   - Root reprovisioning requires at least 4GiB of memory.
+# - timeoutMin: 15
+#   - This test includes a lot of disk I/O and needs a higher
+#     timeout value than the default.
+# - allowConfigWarnings: true
+#   - We intentionally put the root filesystem on partition 5.  This is
+#     legal but usually not intended, so Butane warns about it.
 
 set -xeuo pipefail
 
