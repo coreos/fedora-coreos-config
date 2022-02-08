c4ca6f62fd96164c2a35601a6db97d4d94b02749
Jan Schintag <jan.schintag@de.ibm.com>
2022-02-08 11:42:37 +0100

    tests/kola: Skip incompatible tests on s390x
    These tests have never run successfully on s390x. Skip them for now and
    investigate wether or not they can be adapted for s390x.
    
    Signed-off-by: Jan Schintag <jan.schintag@de.ibm.com>

--- tests/kola/networking/force-persist-ip/test.sh
+++ tests/kola/networking/force-persist-ip/test.sh
@@ -5,14 +5,17 @@ set -xeuo pipefail
 # - kargs provide static network config for eth1 and also coreos.force_persist_ip
 # - ignition provides dhcp network config for eth1
 # Expected result:
-# - with coreos.force_persist_ip ip=kargs win, verify that 
+# - with coreos.force_persist_ip ip=kargs win, verify that
 #   eth1 has the static IP address via kargs
 
 # https://bugzilla.redhat.com/show_bug.cgi?id=1958930#c29
 # - We use net.ifnames=0 to disable consistent network naming here because on
 #   different firmwares (BIOS vs UEFI) the NIC names are different.
 #   See https://github.com/coreos/fedora-coreos-tracker/issues/1060
-# kola: { "platforms": "qemu", "additionalNics": 1, "appendKernelArgs": "ip=10.10.10.10::10.10.10.1:255.255.255.0:myhostname:eth1:none:8.8.8.8 net.ifnames=0 coreos.force_persist_ip" }
+
+# https://github.com/coreos/fedora-coreos-config/issues/1499
+# - Disable the test on s390x
+# kola: { "platforms": "qemu", "additionalNics": 1, "appendKernelArgs": "ip=10.10.10.10::10.10.10.1:255.255.255.0:myhostname:eth1:none:8.8.8.8 net.ifnames=0 coreos.force_persist_ip", "architectures": "!s390x" }
 
 . $KOLA_EXT_DATA/commonlib.sh
 
