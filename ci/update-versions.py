0989a9da761e23bdf2930308e223a6965326a46a
Benjamin Gilbert <bgilbert@redhat.com>
2022-02-11 04:12:28 -0500

    Automatically update component versions every 6 hours
    Push directly to the repo, bypassing pull requests, since this is a
    routine metadata update.  Because we're using the repository's token,
    this bypasses CI on the main branch due to GitHub recursion prevention,
    but that shouldn't affect anything.

---
+++ ci/update-versions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# Update Antora attributes for OS and tool versions.
+
+import os
+import requests
+import sys
+import yaml
+
+GITHUB_RELEASES = {
+    'butane-version': 'coreos/butane',
+    'ignition-version': 'coreos/ignition',
+    'coreos-installer-version': 'coreos/coreos-installer',
+}
+FCOS_STREAMS = {
+    'stable-version': 'stable',
+}
+
+basedir = os.path.normpath(os.path.join(os.path.dirname(sys.argv[0]), '..'))
+
+with open(os.path.join(basedir, 'site.yml'), 'r+') as fh:
+    config = yaml.safe_load(fh)
+    attrs = config.setdefault('asciidoc', {}).setdefault('attributes', {})
+    orig_attrs = attrs.copy()
+
+    for attr, repo in GITHUB_RELEASES.items():
+        resp = requests.get(f'https://api.github.com/repos/{repo}/releases/latest')
+        resp.raise_for_status()
+        tag = resp.json()['tag_name']
+        attrs[attr] = tag.lstrip('v')
+
+    for attr, stream in FCOS_STREAMS.items():
+        resp = requests.get(f'https://builds.coreos.fedoraproject.org/streams/{stream}.json')
+        resp.raise_for_status()
+        # to be rigorous, we should have a separate attribute for each
+        # artifact type, but the website doesn't do that either
+        attrs[attr] = resp.json()['architectures']['x86_64']['artifacts']['metal']['release']
+
+    if attrs != orig_attrs:
+        fh.seek(0)
+        fh.truncate()
+        fh.write("# Automatically modified by update-versions.py; comments will not be preserved\n\n")
+        yaml.safe_dump(config, fh, sort_keys=False)
