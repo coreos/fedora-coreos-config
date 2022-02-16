462f51147a014349b1e9e0bb6e9bdc97e1d154e8
Timothée Ravier <tim@siosm.fr>
2022-02-16 12:30:06 +0100

    tests: Ensure that Ignition can preset/enable socket units

--- tests/kola/ignition/systemd-enable-units/test.sh
+++ tests/kola/ignition/systemd-enable-units/test.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 # kola: { "platforms": "qemu-unpriv" }
-
-# This test makes sure that ignition is able to enable instance units.
+# This test makes sure that ignition is able to enable systemd units of
+# different types.
 # https://github.com/coreos/ignition/issues/586
 # https://github.com/systemd/systemd/pull/9901
-
-# We don't need to test this on every platform. If it passes in
-# one place it will pass everywhere.
+# We don't need to test this on every platform. If it passes in one place it
+# will pass everywhere.
 
 set -xeuo pipefail
 
-ok() {
-    echo "ok" "$@"
-}
-
-fatal() {
-    echo "$@" >&2
-    exit 1
-}
+. $KOLA_EXT_DATA/commonlib.sh
 
 # make sure the presets worked and the instantiated unit is enabled
 if [ "$(systemctl is-enabled echo@foo.service)" != 'enabled' ]; then
@@ -30,3 +22,8 @@ if [ "$(journalctl -o cat -u echo@foo.service | sed -n 2p)" != 'foo' ]; then
     fatal "echo@foo.service did not write 'foo' to journal"
 fi
 ok "echo@foo.service ran and wrote 'foo' to the journal"
+
+if [ "$(systemctl is-enabled podman.socket)" != 'enabled' ]; then
+    fatal "podman.socket systemd unit should be enabled"
+fi
+ok "podman.socket systemd unit is enabled"
