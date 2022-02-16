ad35338efe7186344a215b371f91f73eb5b57618
Timothée Ravier <tim@siosm.fr>
2022-02-16 14:28:35 +0100

    tests: Cleanup and convert to new format

--- tests/kola/data/commonlib.sh
+++ tests/kola/data/commonlib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#This is a common library created for the ok & fatal function 
-#and symlinks added to the data/ in each directory
+# This is a common library created for the ok & fatal function and symlinks
+# added to the data/ in each directory
 
 ok() {
     echo "ok" "$@"
--- tests/kola/ignition/systemd-unmasking/test.sh
+++ tests/kola/ignition/systemd-unmasking/test.sh
@@ -4,22 +4,15 @@
 # It just so happens we have masked dnsmasq in FCOS so we can
 # test this by unmasking it.
 
-# We don't need to test this on every platform. If it passes in
-# one place it will pass everywhere.
-# This test is currently scoped to FCOS because `dnsmasq` is not masked
-# on RHCOS.
+# We don't need to test this on every platform. If it passes in one place it
+# will pass everywhere.
+# This test is currently scoped to FCOS because `dnsmasq` is not masked on
+# RHCOS.
 # TODO-RHCOS: determine if any services on RHCOS are masked and adapt test
 
 set -xeuo pipefail
 
-ok() {
-    echo "ok" "$@"
-}
-
-fatal() {
-    echo "$@" >&2
-    exit 1
-}
+. $KOLA_EXT_DATA/commonlib.sh
 
 # make sure the systemd unit (dnsmasq) is unmasked and enabled
 if [ "$(systemctl is-enabled dnsmasq.service)" != 'enabled' ]; then
