870cfa751cef46ba085d8ab8087ad506b2aaea0f
Jan Schintag <jan.schintag@de.ibm.com>
2022-03-24 15:12:27 +0100

    tests/kola/networking: Update tests with description of kola arguments
    Add a description for the kola arguments.
    Switch the s390x issue for the RFE link.
    
    Signed-off-by: Jan Schintag <jan.schintag@de.ibm.com>

--- tests/kola/networking/force-persist-ip/test.sh
+++ tests/kola/networking/force-persist-ip/test.sh
@@ -9,13 +9,21 @@ set -xeuo pipefail
 #   eth1 has the static IP address via kargs
 
 # https://bugzilla.redhat.com/show_bug.cgi?id=1958930#c29
-# - We use net.ifnames=0 to disable consistent network naming here because on
-#   different firmwares (BIOS vs UEFI) the NIC names are different.
-#   See https://github.com/coreos/fedora-coreos-tracker/issues/1060
 
-# https://github.com/coreos/fedora-coreos-config/issues/1499
-# - Disable the test on s390x
 # kola: { "platforms": "qemu", "additionalNics": 1, "appendKernelArgs": "ip=10.10.10.10::10.10.10.1:255.255.255.0:myhostname:eth1:none:8.8.8.8 net.ifnames=0 coreos.force_persist_ip", "architectures": "!s390x" }
+#
+# - platforms: qemu
+#   - This test should pass everywhere if it passes anywhere.
+# - additionalNics: 1
+#   - Add 1 NIC for this test
+# - appendKernelArgs: "ip=10.10.10.10::10.10.10.1:255.255.255.0:myhostname:eth1:none:8.8.8.8 net.ifnames=0 coreos.force_persist_ip"
+#   - The functionality we're testing here and the configuration for the NIC
+#   - We use net.ifnames=0 to disable consistent network naming here because on
+#     different firmwares (BIOS vs UEFI) the NIC names are different.
+#     See https://github.com/coreos/fedora-coreos-tracker/issues/1060
+# - architectures: !s390x
+#   - appendKernelArgs doesn't work on s390x
+#   - https://github.com/coreos/coreos-assembler/issues/2776
 
 . $KOLA_EXT_DATA/commonlib.sh
 
