94f1bfd8c463b280f385737e89165cd9306aad49
Jonathan Lebon <jonathan@jlebon.com>
2022-04-06 15:13:15 -0400

    tests/kola/kubernetes/kube-watch: stop journal grepping
    The way we check the systemd unit status is racy. Instead, wait up to 30
    seconds for the service to become active.
    
    Instead of grepping the journal, check that a file it should have
    touched was actually touched.
    
    Closes: https://github.com/coreos/fedora-coreos-config/issues/1637

--- tests/kola/kubernetes/kube-watch/test.sh
+++ tests/kola/kubernetes/kube-watch/test.sh
@@ -17,21 +17,22 @@ ok "kube-watch.path successfully activated"
 touch /etc/kubernetes/kubeconfig
 ok "successfully created /etc/kubernetes/kubeconfig"
 
-# If we check the status too soon it could still be activating..
-# Sleep in a loop until it's done "activating"
-while [ "$(systemctl is-active kube-watch.service)" == "activating" ]; do
-    echo "kube-watch is activating. sleeping for 1 second"
+# Give the service 30 seconds to activate
+active=0
+for i in {1..30}; do
+    if systemctl is-active kube-watch.service; then
+        active=1
+        break
+    fi
     sleep 1
 done
-if [ "$(systemctl is-active kube-watch.service)" != "active" ]; then
+if [[ $active != 1 ]]; then
+    systemctl status kube-watch.service
     fatal "kube-watch.service did not successfully activate"
 fi
 ok "kube-watch.service activated successfully"
 
-# NOTE: we've observed a race where the journal message shows up as
-# coming from `echo` rather than `kube-watch`, so we're embedding
-# a UUID in the message to make it easier to find.
-if ! journalctl -o cat -b | grep 27a259a8-7f2d-4144-8b8f-23dd201b630c; then
-    fatal "kube-watch.service did not print message to journal"
+if ! test -e /var/tmp/kube-watched; then
+    fatal "/var/tmp/kube-watched does not exist"
 fi
-ok "Found message from kube-watch.service in journal"
+ok "/var/tmp/kube-watched exists"
