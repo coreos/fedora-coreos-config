59ca34ef38ce578b9a510430d5551960d232880b
Benjamin Gilbert <bgilbert@redhat.com>
2022-05-06 07:11:34 -0400

    tests/kola/ignition: finish moving S3 test fixtures to a new bucket
    We had a couple hardcoded checks that weren't caught.
    
    Fixes: 275f85c3a4d8 ("tests/kola/ignition: move S3 test fixtures to a new bucket")

--- tests/kola/ignition/resource/s3/test.sh
+++ tests/kola/ignition/resource/s3/test.sh
@@ -13,13 +13,13 @@ fi
 
 # verify that the objects are inaccessible anonymously
 for obj in authenticated authenticated.ign; do
-    if curl -sf "https://rh-kola-fixtures.s3.amazonaws.com/resources/$obj"; then
+    if curl -sf "https://ignition-test-fixtures.s3.amazonaws.com/resources/$obj"; then
         fatal "anonymously fetching authenticated resource should have failed, but did not"
     fi
 done
 
 # ...but that the anonymous object is accessible
-if ! curl -sf "https://rh-kola-fixtures.s3.amazonaws.com/resources/anonymous" > /dev/null; then
+if ! curl -sf "https://ignition-test-fixtures.s3.amazonaws.com/resources/anonymous" > /dev/null; then
     fatal "anonymous resource is inaccessible"
 fi
 
