84bc2306fab0a637b715b4fce6b0f4e816a6e39b
Dusty Mabe <dusty@dustymabe.com>
2023-05-04 15:58:53 -0400

    overlay.d/05core: add ifname= karg udev rule propagation
    This adds to coreos-teardown-initramfs.{service,sh} the ability to
    propagate udev rules that were generated by ifname-genrules.sh [1]
    to the real root of the machine to allow for an ephemeral ifname=
    karg on the first boot to have persistent configuration effect (similar
    to our other networking kernel arguments).
    
    This also adds tests for both the firstboot and everyboot case for
    the ifname= karg.
    
    Fixes https://github.com/coreos/fedora-coreos-tracker/issues/553
    
    [1] https://github.com/dracutdevs/dracut/blob/5c3d0a96473ac339fa2d1b25213b8f301c1cfd0d/modules.d/40network/ifname-genrules.sh

---
+++ tests/kola/networking/ifname-karg/data/ifname-karg-lib.sh
@@ -0,0 +1,36 @@
+# This is a library created for our ifname-karg tests
+
+. $KOLA_EXT_DATA/commonlib.sh
+
+# check IP for given NIC name
+check_ip() {
+    # User provides the NIC name.
+    local nic_name=$1
+    # The expected IP is the first one in the range given out by QEMU
+    # user mode networking: https://www.qemu.org/docs/master/system/devices/net.html#using-the-user-mode-network-stack
+    local expected_ip="10.0.2.15"
+    # Verify the given nic name has the expected IP.
+    local nic_ip=$(get_ipv4_for_nic ${nic_name})
+    if [ "${nic_ip}" != "${expected_ip}" ]; then
+        fatal "Error: get ${nic_name} ip = ${nic_ip}, expected is ${expected_ip}"
+    fi
+    ok "get ${nic_name} ip is ${expected_ip}"
+}
+
+# simple file existence check
+check_file_exists() {
+    local file=$1
+    if [ ! -f $file ]; then
+        fatal "expected file ${file} doesn't exist on disk"
+    fi
+    ok "expected file ${file} exists on disk"
+}
+
+# simple file non-existence check
+check_file_not_exists() {
+    local file=$1
+    if [ -f $file ]; then
+        fatal "expected file ${file} to not exist on disk"
+    fi
+    ok "file ${file} does not exist on disk"
+}
