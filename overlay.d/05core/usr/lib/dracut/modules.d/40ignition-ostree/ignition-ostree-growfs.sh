068d1257ddef19b7e2ea4bbb5fffb855035d9bd5
Jonathan Lebon <jonathan@jlebon.com>
2024-07-12 11:51:11 -0400

    overlay/growfs: use luksDump JSON output for analysis
    It's cleaner and less prone to false positives.

--- overlay.d/05core/usr/lib/dracut/modules.d/40ignition-ostree/ignition-ostree-growfs.sh
+++ overlay.d/05core/usr/lib/dracut/modules.d/40ignition-ostree/ignition-ostree-growfs.sh
@@ -129,14 +129,14 @@ while true; do
         crypt)
             # lsblk doesn't print PKNAME of crypt devices with --nodeps
             PKNAME=/dev/$(ls "/sys/dev/block/${MAJMIN}/slaves")
-            LUKS_DUMP=$(cryptsetup luksDump "$PKNAME")
-            if grep -q clevis <<< "$LUKS_DUMP"; then
+            LUKS_DUMP=$(cryptsetup luksDump "$PKNAME" --dump-json-metadata)
+            if jq -e '[.tokens[].type] | index("clevis")' <<< "$LUKS_DUMP"; then
                 # XXX: yuck... we need to expose this sanely in clevis
                 (. /usr/bin/clevis-luks-common-functions
                  eval $(udevadm info --query=property --export "${NAME}")
                  clevis_luks_unlock_device "${PKNAME}" | cryptsetup resize -d- "${DM_NAME}"
                 )
-            elif grep -q "cipher: paes" <<< "$LUKS_DUMP"; then
+            elif jq -e '.segments["0"].encryption | startswith("paes")' <<< "$LUKS_DUMP"; then
                 # CEX LUKS volume: https://github.com/coreos/ignition/issues/1693
                 cryptsetup resize root --key-file /etc/luks/cex.key
             else
