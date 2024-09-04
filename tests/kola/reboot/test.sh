bc0cf2fa020460e20a51cc835f52c3efa27711b8
Dusty Mabe <dusty@dustymabe.com>
2024-09-04 16:22:18 -0400

    tests/reboot: refactor checking code into functions
    This makes it a bit easier to read and also means we don't
    have to run the checks on every boot.

--- tests/kola/reboot/test.sh
+++ tests/kola/reboot/test.sh
@@ -9,46 +9,58 @@ set -xeuo pipefail
 # shellcheck disable=SC1091
 . "$KOLA_EXT_DATA/commonlib.sh"
 
-# /var
-varsrc=$(findmnt -nvr /var -o SOURCE)
-rootsrc=$(findmnt -nvr /sysroot -o SOURCE)
-[[ $(realpath "$varsrc") == $(realpath "$rootsrc") ]]
-ok "/var is backed by rootfs"
+first_boot_sanity_check() {
+    # /var
+    varsrc=$(findmnt -nvr /var -o SOURCE)
+    rootsrc=$(findmnt -nvr /sysroot -o SOURCE)
+    [[ $(realpath "$varsrc") == $(realpath "$rootsrc") ]]
+    ok "/var is backed by rootfs"
 
-# sanity-check that boot is mounted by UUID
-if ! systemctl cat boot.mount | grep -q What=/dev/disk/by-uuid; then
-  systemctl cat boot.mount
-  fatal "boot mounted not by UUID"
-fi
-ok "boot mounted by UUID"
+    # check that boot is mounted by UUID
+    if ! systemctl cat boot.mount | grep -q What=/dev/disk/by-uuid; then
+        systemctl cat boot.mount
+        fatal "boot mounted not by UUID"
+    fi
+    ok "boot mounted by UUID"
 
-# check that we took ownership of the bootfs
-[ -f /boot/.root_uuid ]
+    # check that we took ownership of the bootfs
+    [ -f /boot/.root_uuid ]
 
-# s390x does not have grub, skip this part
+    # check for the UUID dropins
+    if [ "$(arch)" == "ppc64le" ] || [ "$(arch)" == "s390x" ]; then
+        echo "skipping EFI verification on arch $(arch)"
+    else
+        [ -f /boot/grub2/bootuuid.cfg ]
+        mount -o ro /dev/disk/by-label/EFI-SYSTEM /boot/efi
+        found_bootuuid="false"
+        for f in /boot/efi/EFI/*/bootuuid.cfg; do
+            if [ -f "$f" ]; then
+                found_bootuuid="true"
+            fi
+        done
+        if [[ "${found_bootuuid}" == "false" ]]; then
+            fatal "No /boot/efi/EFI/*/bootuuid.cfg found"
+        fi
+        umount /boot/efi
+    fi
+}
 
-if [ "$(arch)" == "ppc64le" ] || [ "$(arch)" == "s390x" ]; then
-  echo "skipping EFI verification on arch $(arch)"
-else
-  # check for the UUID dropins
-  [ -f /boot/grub2/bootuuid.cfg ]
-  mount -o ro /dev/disk/by-label/EFI-SYSTEM /boot/efi
-  found_bootuuid="false"
-  for f in /boot/efi/EFI/*/bootuuid.cfg; do
-      if [ -f "$f" ]; then
-          found_bootuuid="true"
-      fi
-  done
-  if [[ "${found_bootuuid}" == "false" ]]; then
-      fatal "No /boot/efi/EFI/*/bootuuid.cfg found"
-  fi
-  umount /boot/efi
-fi
+subsequent_boot_sanity_check() {
+    # check for expected root=UUID= and boot=UUID= kargs that were set up on
+    # first boot for use in subsequent boots.
+    grep root=UUID="$(cat /boot/.root_uuid)" /proc/cmdline
+    ok "found root karg"
+    bootsrc=$(findmnt -nvr /boot -o SOURCE)
+    eval $(blkid -p -o export "${bootsrc}")
+    grep boot=UUID="${UUID}" /proc/cmdline
+    ok "found boot karg"
+}
 
 # Boot 10 times to find any issues with multiple boots
 # and then verify expected settings on the last boot.
 case "${AUTOPKGTEST_REBOOT_MARK:-}" in
   "")
+      first_boot_sanity_check
       ok "first boot"
       /tmp/autopkgtest-reboot 2
       ;;
@@ -57,15 +69,8 @@ case "${AUTOPKGTEST_REBOOT_MARK:-}" in
       /tmp/autopkgtest-reboot "$((${AUTOPKGTEST_REBOOT_MARK} + 1))"
       ;;
   10)
+      subsequent_boot_sanity_check
       ok "boot 10"
-      # check for expected default kargs
-      grep root=UUID="$(cat /boot/.root_uuid)" /proc/cmdline
-      ok "found root karg"
-
-      bootsrc=$(findmnt -nvr /boot -o SOURCE)
-      eval $(blkid -p -o export "${bootsrc}")
-      grep boot=UUID="${UUID}" /proc/cmdline
-      ok "found boot karg"
       ;;
   *) fatal "unexpected mark: ${AUTOPKGTEST_REBOOT_MARK}";;
 esac
