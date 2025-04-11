43f508950c70d902c4a75ec7788f06e6e1dfc2a3
Dusty Mabe <dusty@dustymabe.com>
2025-04-11 12:14:08 -0400

    Move to Fedora Linux 42

--- ci/buildroot/install-buildroot.sh
+++ ci/buildroot/install-buildroot.sh
@@ -27,10 +27,10 @@ brs=$(grep -v '^#' "${dn}"/buildroot-buildreqs.txt)
  echo "${brs}" | xargs dnf download --source
  # rebuild the SRPM for this arch; see
  # https://bugzilla.redhat.com/show_bug.cgi?id=1402784#c6
- # Add workaround if on F41 for https://github.com/coreos/fedora-coreos-tracker/issues/1901
+ # Add workaround if on F42 for https://github.com/coreos/fedora-coreos-tracker/issues/1901
  source /etc/os-release
  workaround=""
- if [ "${VERSION_ID}" == "41" ]; then
+ if [ "${VERSION_ID}" == "42" ]; then
     workaround="--noclean"
  fi
  find . -name '*.src.rpm' -print0 | xargs -0n 1 rpmbuild -rs --nodeps \
