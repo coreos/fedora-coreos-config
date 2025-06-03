e7894dc8522763afb9a0e2ddcf27b824c6f1b1e3
jbtrystram <jbtrystram@redhat.com>
2025-06-03 13:36:47 +0200

    tests/python: fix test to allow python in f43+
    [1] introduced python dependencies and a test to make sure no python was
    introduced in earlier releases. The test intended to be skip for versions
    above 43 but use `verlt`. Update to use `vergte` which will skip the test
    for f43 and above, as intended.
    
    [1] e70fd15dc08ec1ca2234f513275dd18ca0430206

--- tests/kola/python/test.sh
+++ tests/kola/python/test.sh
@@ -9,7 +9,7 @@ set -xeuo pipefail
 # shellcheck disable=SC1091
 . "$KOLA_EXT_DATA/commonlib.sh"
 
-if verlt "$(get_fedora_ver)" 43; then
+if vergte "$(get_fedora_ver)" 43; then
     ok "Skipping python3 dependencies test"
     exit 0
 fi
