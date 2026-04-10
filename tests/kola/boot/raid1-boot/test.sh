979387df8107f42f8eeb9db515ddaf4944df0294
Rolv Apneseth <rolv.apneseth@gmail.com>
2026-04-10 22:55:24 +0100

    Revert "tests: add `raid1-boot` config with `layout`"
    This reverts commit 0acd55b0459906f93d8d85c095fcc1e97176098b.

--- tests/kola/boot/raid1-boot/test.sh
+++ tests/kola/boot/raid1-boot/test.sh
@@ -20,35 +20,6 @@ set -xeuo pipefail
 # shellcheck disable=SC1091
 . "$KOLA_EXT_DATA/commonlib.sh"
 
-check_bootupctl_components() {
-    local arch_type="$1"
-    local components=()
-    local output_file="out.txt"
-
-    # Define components based on the passed architecture
-    case "${arch_type}" in
-        x86_64)  components=("BIOS" "EFI") ;;
-        aarch64) components=("EFI") ;;
-        ppc64le) components=("BIOS") ;;
-        *)
-            echo "Skipped checking for arch: ${arch_type}"
-            return 0
-            ;;
-    esac
-
-    # 1. Test adopt-and-update
-    bootupctl adopt-and-update | tee "${output_file}"
-    for comp in "${components[@]}"; do
-        assert_file_has_content "${output_file}" "Adopted and updated: ${comp}: .*"
-    done
-
-    # 2. Test status
-    bootupctl status | tee "${output_file}"
-    for comp in "${components[@]}"; do
-        assert_file_has_content_literal "${output_file}" "Component ${comp}"
-    done
-}
-
 tmpdir=$(mktemp -d)
 cd "${tmpdir}"
 
@@ -65,6 +36,11 @@ ok "source is XFS on RAID1 device"
 mount -o remount,rw /boot
 rm -f -v /boot/bootupd-state.json
 
-check_bootupctl_components "$(arch)"
+bootupctl adopt-and-update | tee out.txt
+assert_file_has_content out.txt "Adopted and updated: BIOS: .*"
+assert_file_has_content out.txt "Adopted and updated: EFI: .*"
 
-ok "bootupctl adopt-and-update supports RAID1 boot"
+bootupctl status | tee out.txt
+assert_file_has_content_literal out.txt 'Component BIOS'
+assert_file_has_content_literal out.txt 'Component EFI'
+ok "bootupctl adopt-and-update supports multiple EFIs on RAID1"
