a205a16d97af6e14bbfdec47ee4c8b34b25ce688
Rolv Apneseth <rolv.apneseth@gmail.com>
2026-04-16 13:53:25 +0100

    tests: split raid1-boot test by architecture to avoid failing on s390x

---
+++ tests/kola/boot/raid1-boot/aarch64/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+## kola:
+##   # additionalDisks is only supported on qemu.
+##   platforms: qemu
+##   # Root reprovisioning requires at least 4GiB of memory.
+##   minMemory: 4096
+##   # Linear RAID is setup on these disks.
+##   additionalDisks: ["10G"]
+##   # This test includes a lot of disk I/O and needs a higher
+##   # timeout value than the default.
+##   timeoutMin: 15
+##   architectures: "aarch64"
+##   # This test reprovisions the boot and rootfs.
+##   tags: reprovision
+##   description: Verify updating multiple EFIs using RAID 1 works.
+##   creationDate: 2026-05-06
+
+# shellcheck disable=SC1091
+. "$KOLA_EXT_DATA/commonlib_raid1.sh"
+
+check_raid1_boot
