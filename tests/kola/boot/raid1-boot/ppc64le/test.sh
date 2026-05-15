b9fc744b75a28eb1624633ad98637835831ceff7
Rolv Apneseth <rolv.apneseth@gmail.com>
2026-05-15 10:47:34 +0100

    tests: add raid1-boot test for ppc64le

---
+++ tests/kola/boot/raid1-boot/ppc64le/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+## kola:
+##   # additionalDisks is only supported on qemu.
+##   platforms: qemu
+##   # Root reprovisioning requires at least 4GiB of memory.
+##   minMemory: 4096
+##   # Linear RAID is setup on these disks.
+##   additionalDisks: ["10G"]
+##   # This test includes a lot of disk I/O and needs a higher
+##   # timeout value than the default.
+##   timeoutMin: 15
+##   architectures: "ppc64le"
+##   description: Verify updating the bootloader while using RAID 1 works.
+##   creationDate: 2026-06-11
+
+# shellcheck disable=SC1091
+. "$KOLA_EXT_DATA/commonlib_raid1.sh"
+
+check_raid1_boot
