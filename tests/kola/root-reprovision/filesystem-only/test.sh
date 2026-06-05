5cb9de3aea6cc6203e9394f726dcf75a1a335a13
Dusty Mabe <dusty@dustymabe.com>
2026-06-05 16:01:05 -0400

    tests: remove reprovision tag from kola tests
    With resource based scheduling [1] we should now be able to run all
    the tests together and not split them up into separate !reprovision
    and reprovision runs.
    
    [1] https://github.com/coreos/coreos-assembler/pull/4470

--- tests/kola/root-reprovision/filesystem-only/test.sh
+++ tests/kola/root-reprovision/filesystem-only/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ## kola:
-##   # This test reprovisions the rootfs.
-##   tags: "platform-independent reprovision"
+##   tags: "platform-independent"
 ##   # Root reprovisioning requires at least 4GiB of memory.
 ##   minMemory: 4096
 ##   # This test includes a lot of disk I/O and needs a higher
